f84d86481f1e7507950e58a28f73bff112164072
usaywook <tpdnr1360@snu.ac.kr>
2024-09-04 13:56:18 +0900

    reset default arg

--- algos/CPO.py
+++ algos/CPO.py
@@ -48,14 +48,14 @@ def collect_samples(pid, queue, env, policy,
         # this is to apply different stochasity
         # in replicate of main process
         torch.manual_seed(seed + pid)
-
+    
     while num_steps < min_batch_size:
         t0 = time.time()
         state, info = env.reset(seed=seed)
         t1 = time.time()
         if running_state is not None:
             state = running_state(state)
-
+        
         reward_episode = 0
         env_reward_episode = 0
         env_cost_episode = 0
@@ -70,7 +70,7 @@ def collect_samples(pid, queue, env, policy,
                 else:
                     # Stochastic action
                     action = policy.select_action(state_var)[0].numpy()
-
+    
             action = int(action) if policy.is_disc_action else action.astype(np.float64)
             next_state, reward, cost, done, truncated, _= env.step(action)
 
@@ -98,9 +98,9 @@ def collect_samples(pid, queue, env, policy,
         env_total_reward += env_reward_episode
         env_total_cost += env_cost_episode
         min_reward = min(min_reward, env_reward_episode)
-        max_reward = max(max_reward, env_reward_episode)
-
-
+        max_reward = max(max_reward, env_reward_episode) 
+        
+    
     log['num_steps'] = num_steps
     log['num_episodes'] = num_episodes
     log['env_total_reward'] = env_total_reward
@@ -110,7 +110,7 @@ def collect_samples(pid, queue, env, policy,
     log['max_reward'] = max_reward
     log['min_reward'] = min_reward
     log['env_reward_ep_list'] = env_reward_episode_list
-
+    
     if queue is not None:
         queue.put([pid, memory, log])
     else:
@@ -121,7 +121,7 @@ def merge_log(log_list):
     log = dict()
     total_rewards_episodes = []
 
-    # merge env reward
+    # merge env reward 
     log['env_total_reward'] = sum([x['env_total_reward'] for x in log_list])
     log['env_total_cost'] = sum([x['env_total_cost'] for x in log_list])
     log['num_episodes'] = sum([x['num_episodes'] for x in log_list])
@@ -142,7 +142,7 @@ def merge_log(log_list):
     reward_episode_list_variance = reward_episode_list_sum / log['num_episodes']
     reward_episode_list_std = np.sqrt(reward_episode_list_variance)
     log['std_reward']  = reward_episode_list_std
-
+    
     return log
 
 def trpo_problem(param_size, dtype):
@@ -203,7 +203,7 @@ class CPO:
 
         self.loss_fn = torch.nn.MSELoss()
         # value network is usually set higher lr than policy since it is a bottleneck in learning
-        if args.bfgs_iter_num == -1:
+        if args.bfgs_iter_num is not None:
             self.value_optimizer = torch.optim.LBFGS(self.value_net.parameters(), lr=args.critic_lr, max_iter=args.bfgs_iter_num)
             self.cost_optimizer = torch.optim.LBFGS(self.cost_net.parameters(), lr=args.critic_lr, max_iter=args.bfgs_iter_num)
         else:
@@ -213,7 +213,7 @@ class CPO:
     def line_search(self, model, reward_f, cost_f, x, fullstep, expected_reward_improve_full, expected_cost_improve_full, max_backtracks=10, accept_ratio=0.1):
         reward_fval = reward_f(True).item()
         cost_fval = cost_f(True).item()
-
+        
         for stepfrac in [.5**x for x in range(max_backtracks)]:
             x_new = x + stepfrac * fullstep
             set_flat_params_to(model, x_new)
@@ -221,7 +221,7 @@ class CPO:
             reward_fval_new = reward_f(True).item()
             cost_fval_new = cost_f(True).item()
 
-            actual_reward_improve = reward_fval - reward_fval_new
+            actual_reward_improve = reward_fval - reward_fval_new  
             actual_cost_improve = cost_fval - cost_fval_new
 
             expected_reward_improve = expected_reward_improve_full * stepfrac
@@ -231,9 +231,8 @@ class CPO:
             c_ratio = actual_cost_improve / expected_cost_improve
             if torch.norm(stepfrac * fullstep) <= self.args.max_kl and r_ratio > accept_ratio and c_ratio > accept_ratio:
                 return True, x_new
-        print('Line search Failed')
         return False, x
-
+    
     def collect_samples(self, env, seed):
         t_start = time.time()
         to_device(torch.device('cpu'), self.policy_net)
@@ -245,10 +244,10 @@ class CPO:
             workers.append(multiprocessing.Process(target=collect_samples, args=worker_args))
         for worker in workers:
             worker.start()
-        memory, log = collect_samples(0, None, env, self.policy_net,
-                                      self.mean_action, self.running_state,
+        memory, log = collect_samples(0, None, env, self.policy_net, 
+                                      self.mean_action, self.running_state, 
                                       thread_batch_size, self.args.time_horizon, seed)
-
+        
 
         worker_logs = [None] * len(workers)
         worker_memories = [None] * len(workers)
@@ -284,7 +283,7 @@ class CPO:
         reward_advantages, reward_returns = estimate_advantages(rewards, masks, reward_values.clone().detach(), self.args.gamma, self.args.tau, self.device)
         cost_advantages, cost_returns = estimate_advantages(costs, masks, cost_values.clone().detach(), self.args.gamma, self.args.tau, self.device)
         constraint_value = estimate_constraint_value(costs, masks, self.args.gamma, self.device)
-
+        
         reward_value_loss = self.loss_fn(reward_values, reward_returns)
         cost_value_loss = self.loss_fn(cost_values, cost_returns)
 
@@ -310,7 +309,7 @@ class CPO:
                 # gradient clipping for stability
                 torch.nn.utils.clip_grad_norm_(self.cost_net.parameters(), max_norm=1.0)
                 return c_loss
-
+            
             self.value_optimizer.step(r_closure)
             self.cost_optimizer.step(c_closure)
         else:
@@ -333,7 +332,7 @@ class CPO:
                 log_probs = self.policy_net.get_log_prob(states, actions)
                 action_loss = -reward_advantages * torch.exp(log_probs - fixed_log_probs)
                 return action_loss.mean()
-
+            
         """define the cost loss function for TRPO"""
         def get_cost_loss(volatile=False):
             with torch.set_grad_enabled(not volatile):
@@ -354,7 +353,7 @@ class CPO:
         """Gradient normalization"""
         if self.args.grad_norm:
             loss_reward_grad = loss_reward_grad / torch.norm(loss_reward_grad)
-            loss_cost_grad = loss_cost_grad / torch.norm(loss_cost_grad)
+            loss_cost_grad = loss_cost_grad / torch.norm(loss_cost_grad)   
 
         """DP"""
         g = loss_reward_grad.clone().detach()
@@ -364,11 +363,9 @@ class CPO:
         max_constraint = torch.tensor(self.args.max_constraint, dtype=self.dtype)
 
         b = (constraint_value[0] - max_constraint).to(self.device)
-        print('b: ', b)
-
         if self.args.anneal:
             self.args.max_constraint -= self.args.annealing_factor * self.args.max_constraint
-
+            
         try:
             step, = self.cpo_problem(g, max_kl, a, b)
             #print('step mean: ', abs(step).mean())
@@ -401,7 +398,7 @@ class CPO:
             sample_time = 0
             seed = np.random.randint(1,2**20)
             batch, log = self.collect_samples(self.envs[-1], seed)
-
+            
             sample_time += log['sample_time']
 
             t1 = time.time()
@@ -410,12 +407,12 @@ class CPO:
 
             self.running_state.fix = True
             self.mean_action=True
-
+            
             seed = np.random.randint(1,2**20)
             eval_batch, eval_log = self.collect_samples(self.envs[-1], seed)
             sample_time += eval_log['sample_time']
 
-            self.running_state.fix = False
+            self.running_state.fix = False 
             self.mean_action=False
 
             # calculate values
@@ -425,8 +422,8 @@ class CPO:
 
             meta_avg_cost.append(eval_cost)
 
-            print('{}\tT_sample {:.4f}  T_update {:.4f}\tC_avg/iter {:.2f}  Test_C_avg {:.2f}\tR_avg {:.2f}\tTest_R_avg {:.2f}\tTest_R_std {:.2f}'.format(
-            m_iter, sample_time, t2-t1, np.average(meta_avg_cost), eval_cost, log['env_avg_reward'], eval_log['env_avg_reward'], eval_log['std_reward']))
+            print('{}\tT_sample {:.4f}  T_update {:.4f}\tC_avg/iter {:.2f}  Test_C_avg {:.2f}\tR_avg {:.2f}\tTest_R_avg {:.2f}\tTest_R_std {:.2f}'.format( 
+            m_iter, sample_time, t2-t1, np.average(meta_avg_cost), eval_cost, log['env_avg_reward'], eval_log['env_avg_reward'], eval_log['std_reward']))   
 
             writer.add_scalar('MetaTesting/rewards', eval_log['env_avg_reward'], m_iter)
             writer.add_scalar('MetaTesting/costs', eval_cost, m_iter) 
@@ -436,14 +433,14 @@ class CPO:
 
         writer.close()
         return meta_avg_cost
-
+      
     def train_CPO(self, writer, save_info_obj):
         # variables and lists for recording losses
         v_loss_list = []
         c_loss_list = []
         reward_loss_list = []
         cost_loss_list = []
-
+        
         # lists for dumping plotting data for agent
         rewards_std = []
         env_avg_reward = []
@@ -453,29 +450,29 @@ class CPO:
         total_num_steps = []
         tne = 0 #cummulative number of episodes
         tns = 0 #cummulative number of steps
-
+        
         # lists for dumping plotting data for mean agent
         eval_avg_reward = []
         eval_avg_reward_std = []
         iter_for_best_avg_reward = None
-
+        
         eval_avg_cost = []
 
         # for saving the best model
         best_avg_reward = 0
         best_std = 5
-
+        
         if self.args.model_path is not None:
             total_iterations = self.args.max_iter_num - self.args.update_iter_num
             print('total iterations: ',self.args.max_iter_num,
                'updated iteration: ', self.args.update_iter_num,
                'remaining iteration: ', total_iterations)
-        else:
+        else: 
             self.args.update_iter_num = 0
-
+        
         if self.args.is_meta_test:
             meta_avg_cost = self.meta_test(writer)
-            return
+            return 
 
         for i_iter in range(self.args.update_iter_num, self.args.max_iter_num):
             """generate multiple trajectories that reach the minimum batch_size"""
@@ -493,14 +490,14 @@ class CPO:
             c_loss_list.append(c_loss)
             reward_loss_list.append(reward_loss)
             cost_loss_list.append(cost_loss)
-            rewards_std.append(log['std_reward'])
+            rewards_std.append(log['std_reward']) 
             env_avg_reward.append(log['env_avg_reward'])
             num_of_steps.append(log['num_steps'])
             num_of_episodes.append(log['num_episodes'])
             tne = tne + log['num_episodes']
             tns = tns + log['num_steps']
             total_num_episodes.append(tne)
-            total_num_steps.append(tns)
+            total_num_steps.append(tns)          
 
             # evaluate the current policy
             self.running_state.fix = True  #Fix the running state
@@ -526,15 +523,15 @@ class CPO:
             writer.add_scalar('CriticLoss/cost_value_loss', c_loss, i_iter)
             writer.add_scalar('PolicyLoss/reward_advantage_loss', reward_loss, i_iter)
             writer.add_scalar('PolicyLoss/cost_advantage_loss', cost_loss, i_iter)
-            writer.add_scalar('Training/rewards', eval_log['env_avg_reward'], i_iter)
-            writer.add_scalar('Training/costs', eval_cost, i_iter)
+            writer.add_scalar('Training/rewards', eval_log['env_avg_reward'], i_iter)  
+            writer.add_scalar('Training/costs', eval_cost, i_iter)  
             writer.add_scalar('Training/reward_std', eval_log['std_reward'], i_iter)
 
-            # print learning data on screen
+            # print learning data on screen     
             if i_iter % self.args.log_interval == 0:
-                print('{}\tT_sample {:.4f}  T_update {:.4f}\tC_avg/iter {:.2f}  Test_C_avg {:.2f}\tR_avg {:.2f}\tTest_R_avg {:.2f}\tTest_R_std {:.2f}'.format(
-                    i_iter, log['sample_time'], t1-t0, np.average(eval_avg_cost), eval_cost, log['env_avg_reward'], eval_log['env_avg_reward'], eval_log['std_reward']))
-
+                print('{}\tT_sample {:.4f}  T_update {:.4f}\tC_avg/iter {:.2f}  Test_C_avg {:.2f}\tR_avg {:.2f}\tTest_R_avg {:.2f}\tTest_R_std {:.2f}'.format( 
+                    i_iter, log['sample_time'], t1-t0, np.average(eval_avg_cost), eval_cost, log['env_avg_reward'], eval_log['env_avg_reward'], eval_log['std_reward']))              
+            
             # save the best model
             if eval_log['env_avg_reward'] >= best_avg_reward and eval_log['std_reward'] <= best_std:
                 print('Saving new best model !!!!')
@@ -543,22 +540,22 @@ class CPO:
                 to_device(self.device, self.policy_net, self.value_net, self.cost_net)
                 best_avg_reward = eval_log['env_avg_reward']
                 best_std = eval_log['std_reward']
-                iter_for_best_avg_reward = i_iter+1
+                iter_for_best_avg_reward = i_iter+1  
 
             # save some intermediate models to sample trajectories from
             if self.args.save_intermediate_model > 0 and (i_iter+1) % self.args.save_intermediate_model == 0:
                 to_device(torch.device('cpu'), self.policy_net, self.value_net, self.cost_net)
                 save_info_obj.save_intermediate_models(self.policy_net, self.value_net, self.cost_net, self.running_state, self.args, i_iter)
                 to_device(self.device, self.policy_net, self.value_net, self.cost_net)
-
+            
             """clean up gpu memory"""
             torch.cuda.empty_cache()
 
         meta_avg_cost = self.meta_test(writer)
-
+            
         # dump expert_avg_reward, num_of_steps, num_of_episodes
-        save_info_obj.dump_lists(best_avg_reward, num_of_steps, num_of_episodes, total_num_episodes,
+        save_info_obj.dump_lists(best_avg_reward, num_of_steps, num_of_episodes, total_num_episodes, 
                                  total_num_steps, rewards_std, env_avg_reward, v_loss_list, c_loss_list, reward_loss_list, cost_loss_list,
                                  eval_avg_reward, eval_avg_reward_std, eval_avg_cost, meta_avg_cost)
         print("Best iter: ", iter_for_best_avg_reward, 'Best eval Reward:', best_avg_reward, "best reward std:", best_std)
-        return best_avg_reward, best_std, iter_for_best_avg_reward
+        return best_avg_reward, best_std, iter_for_best_avg_reward 
